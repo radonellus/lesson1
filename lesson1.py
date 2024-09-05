cb114567ee8ce3809e32e31f00255f39201ff798
radonellus <radonellus@gmail.com>
2024-09-05 13:34:34 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+# "1st program"
+print(9**0.5*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#4th program
+print('123.456')
+print(123.456)
+print(123.456*10)
+print(int('123.456'))
